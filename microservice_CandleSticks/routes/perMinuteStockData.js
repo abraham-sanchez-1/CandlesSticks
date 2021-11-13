40c872d7839858364458fc63d1b7da0a92cc9f2d
Abe Sanchez <sanchez95san@gmail.com>
2021-11-12 23:49:16 -0600

    setup route, not completed

--- microservice_CandleSticks/routes/perMinuteStockData.js
+++ microservice_CandleSticks/routes/perMinuteStockData.js
@@ -8,11 +8,31 @@ const config = require('config');
 
 router.get('/', (req,res) => {
   const stockTicker = req.body.stockTicker;
+  //logic to figure out time,. NOTE: Alpaca uses ISOstring
+  const oneMinute = 60000;
+  const systemTime = new Date()
+  const twoMinutesBefore = new Date(systemTime - (2 * oneMinute)).toISOString();
+  const oneMinuteBefore = new Date(systemTime - oneMinute).toISOString();
   const endpoint = config.alpacaEnpoint;
-  return res.json({
-    'Node Host': os.hostname(),
-    'Message': 'CandleSticks Microservice currently up and running'
-  })
+  let response
+  try {
+      response = await fetch(`${endpoint}/v1/bars/minute?symbol=${stockTicker}&start${twoMinutesBefore}&end${oneMinuteBefore}`, {
+      headers: {
+        'APCA-API-KEY-ID': process.env.ALPACAID,
+        'APCA-API-SECRET-KEY': process.env.ALPACAAPIKEY
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json(error)
+  }
+  if(response){
+    return res.status(200).json(response)
+  }else{
+    return res.status(400).json({
+      message: 'Successfully sent request to Alpaca but returned no data'
+    })
+  } 
 });
 
 module.exports = router;
