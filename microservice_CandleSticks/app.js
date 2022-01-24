8a64d181c804312c7982df190f6725c03f496a2d
Abe Sanchez <sanchez95san@gmail.com>
2022-01-23 22:16:09 -0600

    first time testing out websockets

--- microservice_CandleSticks/app.js
+++ microservice_CandleSticks/app.js
@@ -2,7 +2,6 @@
 
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
 
 const basePath = '/api/v1'
 
--- microservice_CandleSticks/index.js
+++ microservice_CandleSticks/index.js
@@ -2,9 +2,41 @@
 
 const http = require('http');
 const app = require('./app');
+const {parse} = require('url');
+// const websockets = require('websockets')
+const WebSocket = require('ws');
 
-const httpListenerPort = 4000
+const httpListenerPort = 4000;
 
-const httpServer = http.createServer(app).listen(httpListenerPort, ()=> {
+const httpServer = http.createServer(app);
+
+const wss1 = new WebSocket.Server({ noServer: true});
+
+wss1.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    console.log('received: %s', data);
+    ws.send(`Message received -> ${data}`)
+  });
+  console.log('A user has connected to WebSocket service')
+  ws.send('You have connected to CWS: CandleSticks WebSocket Service');
+});
+
+wss1.on('close', (ws) => {
+  ws.send('Closing CWS Connection');
+})
+
+httpServer.on('upgrade', (request, socket, head) => {
+  const {pathname} = parse(request.url);
+  if(pathname === '/livedata'){
+    wss1.handleUpgrade(request, socket, head, (ws) => {
+      wss1.emit('connection', ws, request);
+    });
+  }else{
+    socket.destroy();
+  }
+})
+
+httpServer.listen(httpListenerPort, ()=> {
   console.log('app is listening on localhost: ' + httpListenerPort);
 });
+
